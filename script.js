52447c4404f2d7330312a70f8217a70e02b808ff
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-05 11:32:23 +0200

    Add newQuote function.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Get Quotes From API
 let apiQuotes = [];
+//Show New Quote
+function newQuote() {
+    //Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
 
 async function getQuotes() {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
@@ -7,12 +13,10 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
+        newQuote()
     } catch (error) {
         // Catch Error Here
-
     }
 }
-
 // On Load
 getQuotes();
