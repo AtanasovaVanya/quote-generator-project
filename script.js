15930b40a7d8572f3540a0b9e712870c328a1dcc
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-05 11:16:39 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get Quotes From API
+let apiQuotes = [];
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes[12]);
+    } catch (error) {
+        // Catch Error Here
+
+    }
+}
+
+// On Load
+getQuotes();
