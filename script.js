e1fd45785f1916b2c43f600d5346a1df55094fab
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-05 12:07:31 +0200

    Add tweetQuote function and button event listeners

--- script.js
+++ script.js
@@ -37,5 +37,16 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
