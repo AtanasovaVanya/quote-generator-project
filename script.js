8de24928903cb59c1e87af0c85d33446a484d399
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-06 11:54:18 +0200

    Add loader to script.js

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Get Quotes From API
 let apiQuotes = [];
-//Show New Quote
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
+// Show New Quote
 function newQuote() {
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if author field is blank and replace it with 'Unknown'
@@ -22,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
+// Get Quotes from an API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
@@ -47,6 +66,8 @@ function tweetQuote() {
 //Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
+
+
 
 // On Load
 getQuotes();
